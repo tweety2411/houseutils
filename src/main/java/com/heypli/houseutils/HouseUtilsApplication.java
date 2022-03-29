3181574c039978c0f9226f42056385ae585ce6ec
tweety1121 <baejisuk524@gmail.com>
2022-03-29 11:01:55 +0900

    11

---
+++ src/main/java/com/heypli/houseutils/HouseUtilsApplication.java
@@ -0,0 +1,12 @@
+package com.heypli.houseutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseUtilsApplication.class, args);
+    }
+}
